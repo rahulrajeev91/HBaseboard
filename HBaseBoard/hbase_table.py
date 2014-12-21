f1923421ebb6613d0d6d01cdcb16a15e271ae902
Rahul Rajeev <hello@rahulrajeev.co>
2014-12-21 15:48:27 +0530

    table scanner can filter on the key based on regex

--- HBaseBoard/hbase_table.py
+++ HBaseBoard/hbase_table.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
+import re
+
 
 class _TableScanner(object):
     def __init__(self, table):
+        self.key_regex = re.compile(".*")
         self.table_scan = table.scan()
 
     def __iter__(self):
         return self
 
     def next(self):
-        val = self.table_scan.next()
-        return val
+        key, val = self.table_scan.next()
+        if self.key_regex.search(key):
+            return (key, val)
+        return self.next()
+
+    def add_key_regex(self, regex):
+        self.key_regex = re.compile(regex)
 
 
 class HBaseTable(object):
@@ -21,5 +29,8 @@ class HBaseTable(object):
         for key, values in insert_list:
             self.table.put(key, values)
 
-    def scan(self):
-        return _TableScanner(self.table)
+    def scan(self, key=None):
+        table_scanner = _TableScanner(self.table)
+        if key:
+            table_scanner.add_key_regex(key)
+        return table_scanner
--- tests/hbase_table_test.py
+++ tests/hbase_table_test.py
@@ -29,7 +29,31 @@ class TestHBaseTableClass(object):
         test_val1 = ("key1", {"cf:first_name": "bugs", "cf:last_name": "bunny"})
         test_val2 = ("key2", {"cf:first_name": "daffy", "cf:last_name": "duck"})
         my_table.put([test_val1, test_val2])
-        table_scanner = my_table.scan()
 
-        assert test_val1 == table_scanner.next()
-        assert test_val2 == table_scanner.next()
+        table_vals = []
+        for value in my_table.scan():
+            table_vals.append(value)
+
+        assert 2 == len(table_vals)
+        assert test_val1 in table_vals
+        assert test_val2 in table_vals
+
+    def test_scan_accepts_filter_on_keys(self):
+        my_table = HBaseTable("test_table", self.hb_wrapper)
+        test_val1 = ("key1_matches",
+                     {"cf:first_name": "bugs", "cf:last_name": "bunny"})
+        test_val2 = ("key2_does_not_match",
+                     {"cf:first_name": "daffy", "cf:last_name": "duck"})
+        test_val3 = ("key3_matches",
+                     {"cf:first_name": "porky", "cf:last_name": "pig"})
+        test_val4 = ("key4_matches_not_expected",
+                     {"cf:first_name": "goofy", "cf:last_name": "dog"})
+        my_table.put([test_val1, test_val2, test_val3, test_val4])
+
+        table_vals = []
+        for value in my_table.scan(key="matches$"):
+            table_vals.append(value)
+
+        assert 2 == len(table_vals)
+        assert test_val1 in table_vals
+        assert test_val3 in table_vals
